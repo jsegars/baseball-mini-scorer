8239c331ae360f63da2b454084f1e766c2f95872
Jeff Segars <jeff.segars@busynoggin.com>
2026-05-12 17:29:15 -0500

    PartyKit: send current state to new connections

--- party/index.js
+++ party/index.js
@@ -1,10 +1,18 @@
 export default class ScorerParty {
   constructor(room) {
     this.room = room
+    this.lastState = null
+  }
+
+  onConnect(connection) {
+    // Send current state to any new joiner
+    if (this.lastState) {
+      connection.send(this.lastState)
+    }
   }
 
   onMessage(message, sender) {
-    // Broadcast any state update to all other connected clients
+    this.lastState = message
     this.room.broadcast(message, [sender.id])
   }
 }
